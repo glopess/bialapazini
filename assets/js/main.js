d45b8c21b0eccae9c7374bbcd17d6a8dfc872ac1
gustavo lopes <gustavo_lsilva@hotmail.com>
2020-04-04 18:59:48 -0300

    rollback de ultima versao com atualizacao do jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,29 +50,3 @@ function openBoxEffects() {
     }, 800);
 }
 
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .click(function(event) {
-    if (
-        
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-        debugger
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-
-        var scrollTop = target.offset().top - $('.sticky-top').height();
-
-        $('html, body').animate({
-          scrollTop: scrollTop
-        }, 1000)
-      }
-    }
-});
